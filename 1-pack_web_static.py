88fe8dac2689c0592cc4ff095cf2acb28b467e30
oscargomez87 <oscarstiven87@gmail.com>
2020-04-09 11:01:42 -0500

    Fabric scripts that compresses web_static folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Fabricfile that moves web_static contents from AirBnB_v2 repo"""
+from fabric.api import local, hide
+from datetime import datetime
+
+def do_pack():
+    """Compresses folder in tgz format"""
+
+    time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    name = "web_static"
+    loc = "versions"
+    with hide('running'):
+        local("mkdir -p versions", capture=False)
+        local("echo \"Packing {} to {}/{}_{}.tgz\""
+              .format(name, loc, name, time))
+    result = local("tar -cvzf {}/{}_{}.tgz web_static"
+                   .format(loc, name, time))
+    if not result.failed:
+        return "{}/{}_{}.tgz".format(loc, name, time)
