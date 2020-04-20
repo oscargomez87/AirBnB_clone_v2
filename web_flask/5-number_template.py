8a531976396c4280bcb76dc698710534ce0d0244
oscargomez87 <oscarstiven87@gmail.com>
2020-04-20 16:18:51 -0500

    Added route "/number_template" that renders template "5-number.html"

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctxt(text):
+    str = text.replace('_', ' ')
+    return 'C {}'.format(str)
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def pythonrt(text):
+    str = text.replace('_', ' ')
+    return 'Python {}'.format(escape(str))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numberrt(n):
+    return 'n is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_templatert(n):
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run()
