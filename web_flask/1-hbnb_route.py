e7731ec4b2653e4884379a131f6929aa0fc920cf
oscargomez87 <oscarstiven87@gmail.com>
2020-04-20 15:18:39 -0500

    pep8 compliance fixes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def hello():
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
 if __name__ == "__main__":
     app.run()
