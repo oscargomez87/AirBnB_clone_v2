fd517f97e5c408405e3c55b3ea8d0034d576c72e
oscargomez87 <oscarstiven87@gmail.com>
2020-04-20 16:06:33 -0500

    Added route "/number/<n>" that returns "n is  a number" only if n is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctxt(text):
+    str = text.replace('_', ' ')
+    return 'C {}'.format(str)
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def pythonrt(text):
+    str = text.replace('_', ' ')
+    return 'Python {}'.format(escape(str))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numberrt(n):
+    return 'n is a number'
+
+
+if __name__ == "__main__":
+    app.run()
