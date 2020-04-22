c638b69e28665ab39013527e1981c4c1a25fab72
oscargomez87 <oscarstiven87@gmail.com>
2020-04-22 13:01:43 -0500

    Added route from script 7 Removed states handled in another route and template

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,19 +12,21 @@ def end(resp_or_exep):
 
 
 @app.route('/states', strict_slashes=False)
+def states():
+    """Loads all states in DB then returns a template using those states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states_list=states)
+
 @app.route('/states/<id>', strict_slashes=False)
 def states_by_id(id=None):
     """Returns a view of all states or an specific state"""
     states = storage.all(State)
-    if id:
-        state_id = "State.{}".format(id)
-        state = {}
-        if state_id in states.keys():
-            state = states[state_id]
-        return render_template('9-states.html', state=state,)
-    else:
-        return render_template('9-states.html', states=states.values())
+    state_id = "State.{}".format(id)
+    state = {}
+    if state_id in states.keys():
+        state = states[state_id]
+    return render_template('9-states.html', state=state)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0')
