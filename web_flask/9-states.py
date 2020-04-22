2b1b2836c04f1d1df5ded28ff9ada79d3c552dee
oscargomez87 <oscarstiven87@gmail.com>
2020-04-22 11:41:50 -0500

    Script that starts a Flask we app

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import *
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def end(resp_or_exep):
+    """close SQLALchemy session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<string:id>', strict_slashes=False)
+def states_by_id(id=None):
+    states = storage.all(State).values()
+    state = {}
+    if id is not None:
+        for item in states:
+            if id == item.id:
+                state = item
+                break
+        return render_template('9-states.html', state=state,)
+    else:
+        return render_template('9-states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run()
