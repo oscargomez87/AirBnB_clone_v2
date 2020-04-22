7223e249d407d56669c5c713005c6c03aeb88aad
oscargomez87 <oscarstiven87@gmail.com>
2020-04-22 13:24:13 -0500

    Script that runs a Flask web application Renders template 10-hbnb_filters.html In progress

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import *
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session():
+    """Closes SQLAlchemy session"""
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Renders a search bar with selectable state and city"""
+    states = storage.all(States).values()
+    return render_template('10-hbnb_filters.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
